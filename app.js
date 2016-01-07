9f9545a4f3bb5ca5e495e1f576023af5a953d090
Chaz Leong <cleong14@gmail.com>
2016-01-06 20:59:06 -1000

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// require in modules
+
+// external modules
+var bodyParser = require('body-parser');
+var express = require('express');
+var jade = require('jade');
+
+// my modules
+
+var app = express();
+
